258f5ddf8817388e01e8af7abac83821a7e114b8
Erin W Dickie <edickie@users.noreply.github.com>
2018-06-23 18:23:53 -0400

    new version number

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(os.path.join(here, 'README.md')) as f:
 
 setup(
     name='ciftify',
-    version='2.0.4',
+    version='2.0.5',
     description='The tools of the Human Connectome Project (HCP) '\
             'adapted for working with non-HCP datasets',
     long_description=long_description,
