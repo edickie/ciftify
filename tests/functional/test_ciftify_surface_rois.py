88d0865ea52cc419e7abe87887b99074a51971c3
Erin Dickie <erin.w.dickie@gmail.com>
2018-07-24 17:24:47 -0400

    fixed bugs in ciftify_rois functional test

--- tests/functional/test_ciftify_surface_rois.py
+++ tests/functional/test_ciftify_surface_rois.py
@@ -19,25 +19,9 @@ right_surface = os.path.join(get_test_data_path(),
         'sub-50005.R.midthickness.32k_fs_LR.surf.gii')
 
 class TestCitifySurfaceRois(unittest.TestCase):
+
     def setUp(self):
         self.path = tempfile.mkdtemp()
-        vertices2_csv = os.path.join(src_data_dir, 'vertices1.csv')
-        vertices1_csv = os.path.join(src_data_dir, 'vertices1.csv')
-
-        with open(vertices1_csv, "w") as text_file:
-            text_file.write('''hemi,vertex
-        L,11801
-        L,26245
-        L,26235
-        L,26257
-        L,13356
-        L,289
-        L,13336
-        L,13337
-        L,26269
-        L,13323
-        L,26204
-        '''
         # create a temp outputdir
 
     def tearDown(self):
@@ -61,13 +45,13 @@ class TestCitifySurfaceRois(unittest.TestCase):
         L,26269
         L,13323
         L,26204
-        '''
+        ''')
 
         output_dscalar = os.path.join(self.path, 'gaussian.dscalar.nii')
         run(['ciftify_surface_rois', vertices1_csv, '10', '--gaussian',
              left_surface,
              right_surface,
-             output_dscalar)])
+             output_dscalar])
         assert os.path.isfile(output_dscalar)
 
 
@@ -82,13 +66,13 @@ class TestCitifySurfaceRois(unittest.TestCase):
         R,2629
         R,29290
         R,29290
-        '''
+        ''')
 
         output_dscalar = os.path.join(self.path, 'probmap.dscalar.nii')
         run(['ciftify_surface_rois', vertices2_csv, '10', '--probmap',
              left_surface,
              right_surface,
-             output_dscalar)])
+             output_dscalar])
         assert os.path.isfile(output_dscalar)
 
     def test_ciftify_surface_rois_with_labels(self):
@@ -104,5 +88,5 @@ class TestCitifySurfaceRois(unittest.TestCase):
                         'S1200.L.midthickness_MSMAll.32k_fs_LR.surf.gii'),
               os.path.join(ciftify.config.find_HCP_S1200_GroupAvg(),
                         'S1200.R.midthickness_MSMAll.32k_fs_LR.surf.gii'),
-              os.path.join(new_outputs, 'rois', 'tvertex.dscalar.nii')])
+              output_dscalar])
         assert os.path.isfile(output_dscalar)
